4bf0401781ced6f485f951a99ede4de73d2940fe
emmanuel2011 <okoyee61@gmail.com>
2025-02-01 05:13:56 +0100

    added .env

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -22,7 +22,7 @@ export default class  ProductListing {
 
     async init() {
         // our dataSource will return a Promise...so we can use await to resolve it.
-        const list = await this.dataSource.getData();
+        const list = await this.dataSource.getData(this.category);
         let limitedList = this.filterList(list);
         this.renderList(limitedList);
     }
