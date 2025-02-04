d7f2534909d0041d734dc9bc912a04a476069eb5
pearlrhema <pearlrhema8@gmail.com>
2025-02-04 11:40:21 +0100

    rendering the porduct page without filtered function

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,3 +1,49 @@
+// import { renderListWithTemplate } from "./utils.mjs";
+
+// function productCardTemplate(product) {
+//   return `<li class="product-card">
+//   <a href="product_pages/index.html?product=${product.Id}">
+//   <img
+//     src="${product.Image}"
+//     alt="Image of ${product.Name}"
+//   />
+//   <h3 class="card__brand">${product.Brand.Name}</h3>
+//   <h2 class="card__name">${product.Name}</h2>
+//   <p></p>
+//   <p class="product-card__price">$${product.FinalPrice}</p></a>
+// </li>`;
+// }
+
+// export default class  ProductListing {
+//     constructor(category, dataSource, listElement){
+//         this.category = category;
+//         this.dataSource = dataSource;
+//         this.listElement = listElement;
+//     }
+
+//     async init() {
+//         // our dataSource will return a Promise...so we can use await to resolve it.
+//         const list = await this.dataSource.getData();
+//         let limitedList = this.filterList(list);
+//         this.renderList(limitedList);
+//     }
+
+//     renderList(list) {
+//         renderListWithTemplate(productCardTemplate, this.listElement, list);
+//     }
+
+//     filterList(list){
+//       let limitedList = [];
+//       list.forEach(element => {
+//         if (element.Id != "989CG" && element.Id != "880RT"){
+//           limitedList.push(element)
+//         };
+//       });
+//       return limitedList;
+//     }
+// }
+
+//code without filtered product list starts here
 import { renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
@@ -9,11 +55,12 @@ function productCardTemplate(product) {
   />
   <h3 class="card__brand">${product.Brand.Name}</h3>
   <h2 class="card__name">${product.Name}</h2>
+  <p></p>
   <p class="product-card__price">$${product.FinalPrice}</p></a>
 </li>`;
 }
 
-export default class  ProductListing {
+export default class ProductListing {
     constructor(category, dataSource, listElement){
         this.category = category;
         this.dataSource = dataSource;
@@ -21,23 +68,12 @@ export default class  ProductListing {
     }
 
     async init() {
-        // our dataSource will return a Promise...so we can use await to resolve it.
+        // Fetch the full list of products without filtering
         const list = await this.dataSource.getData();
-        let limitedList = this.filterList(list);
-        this.renderList(limitedList);
+        this.renderList(list);
     }
 
     renderList(list) {
         renderListWithTemplate(productCardTemplate, this.listElement, list);
-    }
-
-    filterList(list){
-      let limitedList = [];
-      list.forEach(element => {
-        if (element.Id != "989CG" && element.Id != "880RT"){
-          limitedList.push(element)
-        };  
-      });
-      return limitedList;
     }
 }
