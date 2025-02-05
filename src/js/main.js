0b9d868083429cc523f0688cde828749d885de1f
emmanuel2011 <okoyee61@gmail.com>
2025-02-05 17:35:55 +0100

    edited

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,4 @@
 import ProductData from "./ProductData.mjs";
-import ProductList from "./productList.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
