06da514c4d04bd827ea2b829c53f8d38bee0ecc9
emmanuel2011 <okoyee61@gmail.com>
2025-02-06 02:06:06 +0100

    edited

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -5,23 +5,19 @@ function convertToJson(res) {
     throw new Error("Bad Response");
   }
 }
-const baseURL = import.meta.env.VITE_SERVER_URL
 
 export default class ProductData {
   constructor(category) {
-      this.category = category;
-    // this.path = `../json/${this.category}.json`;
-    
+    this.category = category;
+    this.path = `../json/${this.category}.json`;
   }
-  async getData(category) {
-    const response = await fetch(baseURL + `products/search/${category}`);
-    const data = await convertToJson(response);
-    return data.Result;
+  getData() {
+    return fetch(this.path)
+      .then(convertToJson)
+      .then((data) => data);
   }
-  
   async findProductById(id) {
-    const products = await fetch(baseURL + `product/${id}`);
-    const data = await convertToJson(response);
-    return data.Result;
+    const products = await this.getData();
+    return products.find((item) => item.Id === id);
   }
 }
