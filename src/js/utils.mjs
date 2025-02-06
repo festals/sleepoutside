74624123b562c2d3416321ec9334e77ad8fdabd2
emmanuel2011 <okoyee61@gmail.com>
2025-02-06 04:16:03 +0100

    added

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -9,12 +9,10 @@ export function qs(selector, parent = document) {
 export function getLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
 }
-
 // save data to local storage
 export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
-
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
   qs(selector).addEventListener("touchend", (event) => {
@@ -29,29 +27,4 @@ export function getParams(param) {
   const queryString = window.location.search; 
   const urlParams = new URLSearchParams(queryString); 
   return urlParams.get(param); 
-}
-// function to take a list of objects and a template and insert the objects as HTML into the DOM
-
-export function renderListWithTemplate(
-  templateFn,
-  parentElement,
-  list,
-  position = "afterbegin",
-  clear = false
-) {
-  const htmlStrings = list.map(templateFn);
-  // if clear is true we need to clear out the contents of the parent.
-  if (clear) {
-    parentElement.innerHTML = "";
-  }
-  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
-}
-
-// set a listener for both touchend and click
-export function setClick(selector, callback) {
-  qs(selector).addEventListener("touchend", (event) => {
-    event.preventDefault();
-    callback();
-  });
-  qs(selector).addEventListener("click", callback);
 }
