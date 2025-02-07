96ba2dabc79f925fd59c638a6350e52c39a6c085
emmanuel2011 <okoyee61@gmail.com>
2025-02-07 03:05:34 +0100

    added quantity

--- src/js/ShoppingCart.mjs
+++ src/js/ShoppingCart.mjs
@@ -1,32 +1,22 @@
 import { getLocalStorage } from "./utils.mjs";
 
-function cartItemTemplate(item){
+export function cartItemTemplate(item, onQuantityChange){
     return `<li class="cart-card divider">
         <a href="#" class="cart-card__image">
-        <img
-            src="${item.Image}"
-            alt="Image of ${item.Name}"
-        />
+            <img
+                src="${item.Image}"
+                alt="Image of ${item.Name}"
+            />
+        </a>
         <a href="#">
             <h2 class="card__name">${item.Name}</h2>
         </a>
+        
         <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-        <p class="cart-card__quantity">qty: 1</p>
+        <input type="number" id="quantity-${item.id}" class="cart-card__quantity" value="${item.quantity}" min="1" />
+        <label for="quantity-${item.id}" class="cart-card__quantity-label">Qty:</label>
+        
         <p class="cart-card__price">$${item.FinalPrice}</p>
-        </li>
-        `
-}
-
-
-export default class ShoppingCart{
-    constructor(key, parentSelector){
-        this.key = key;
-        this.parentSelector = parentSelector;
-    }
-
-    renderCartContents(){
-      const cartItems = getLocalStorage(this.key);
-      const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-      document.querySelector(this.parentSelector).innerHTML = htmlItems.join(""); 
-    }
+        
+    </li>`;
 }
